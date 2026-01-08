22eee1d4686da5442676e27cef12e991759f3475
wiissal <wissaloa1@gmail.com>
2026-01-08 11:38:46 +0100

    feat: add Jest configuration for testing

---
+++ src/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!src/worker.js',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/*.test.js',
+    '**/*.spec.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
