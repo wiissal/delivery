c8620c4b50ad8bc71883c3483b0bbb46f29b3682
wiissal <wissaloa1@gmail.com>
2026-01-08 12:40:45 +0100

    feat: add delivery routes

---
+++ src/routes/deliveryRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const deliveryController = require('../controllers/deliveryController');
+
+// GET all deliveries 
+router.get('/', deliveryController.getAllDeliveries);
+
+// GET delivery by ID
+router.get('/:id', deliveryController.getDeliveryById);
+
+// POST create new delivery
+router.post('/', deliveryController.createDelivery);
+
+// PUT update delivery status
+router.put('/:id/status', deliveryController.updateDeliveryStatus);
+
+// DELETE delivery
+router.delete('/:id', deliveryController.deleteDelivery);
+
+module.exports = router;
